60faa755ff922583127e0e2922e7f5cec259df71
rooben-me <rubankumar.nkl@gmail.com>
2022-04-13 16:24:27 +0530

    change db URI

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     DB_LOCAL_URI: "mongodb://localhost:27017/travigo",
     DB_URI:
-      "mongodb+srv://ruban:Goodgame6190@cluster0.vbfzh.mongodb.net/bookit?retryWrites=true&w=majority",
+      "mongodb+srv://ruban:Goodgame6190@cluster0.vbfzh.mongodb.net/travigo?retryWrites=true&w=majority",
   },
   images: {
     domains: ["res.cloudinary.com"],
